bdc2f87551d15b1edf71108309308571eb78a293
Fatih Kara <fath.karaa@gmail.com>
2024-04-29 15:51:23 +0300

    finished burcdetail

--- horoscopelist/lib/burc_detay.dart
+++ horoscopelist/lib/burc_detay.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:horoscopelist/model/burc.dart';
 
 class BurcDetay extends StatelessWidget {
-  const BurcDetay({super.key});
+  final Burc secilenBurc;
+  const BurcDetay({required this.secilenBurc, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [SliverAppBar(
+          expandedHeight: 150,
+          pinned: true,
+          title: Text(secilenBurc.burcAdi + " Burcu Özellikleri"),
+          backgroundColor: Colors.pink,
+          flexibleSpace: FlexibleSpaceBar(background: Image.asset('images/' + secilenBurc.burcBuyukResim),centerTitle: true,),
+        ),
+        SliverToBoxAdapter(
+          child: SingleChildScrollView(child: Text(secilenBurc.burcDetayi),),
+        )
+        ],
+      ),
+    );
   }
 }
--- horoscopelist/lib/burc_item.dart
+++ horoscopelist/lib/burc_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:horoscopelist/burc_detay.dart';
 import 'package:horoscopelist/model/burc.dart';
 
 class BurcItem extends StatelessWidget {
@@ -11,6 +12,9 @@ class BurcItem extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Card(
         child: ListTile(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => BurcDetay(secilenBurc: listelenenBurc),));
+          },
           leading: Image.asset("images/" + listelenenBurc.burcKucukResim,width: 64,height: 64,),
           title: Text(listelenenBurc.burcAdi),
           subtitle: Text(listelenenBurc.burcTarihi),
